d46380b2f0042c4ab6bb79489260e328c415c15c
adzibilal <adzibilal02@gmail.com>
2023-10-09 16:50:10 +0700

    first commit

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,5 @@
+const SearchPage = () => {
+    return ( <div className="">search page</div> );
+}
+ 
+export default SearchPage;
---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,9 @@
+import { NavbarRoutes } from "@/components/navbar-route"
+import { MobileSidebar } from "./mobile-sidebar"
+
+export const Navbar = () => {
+    return (<div className='p-4 border-b h-full flex items-center bg-white shadow-sm'>
+        <MobileSidebar/>
+        <NavbarRoutes/>
+    </div>)
+}
